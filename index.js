82fb07a28f9456a86c9cd72625d792089c743cc9
Duc Do <dotrananhduc@gmail.com>
2017-04-01 15:03:08 +0000

    add functions to index.js

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 var express = require("express");
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+function parsetime(time) {
+  // Parse time as ISO
+  return {
+    hour: time.getHours(),  
+    minute: time.getMinutes(),  
+    second: time.getSeconds()
+  };
+}
+
+function unixtime(time) {
+  // Parse time as unix time
+  return {
+    unixtime : time.getTime()
+  };
+}
+
+app.get('/:query', function (req, res) {
+  var unix = null;
+  var natural = null;
+  var rs = {"unix": unix, "natural": natural};
+  res.send(rs);
 })
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 8080!');
 })
