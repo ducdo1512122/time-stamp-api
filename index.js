148f5740bbbc035c196bf769303bddfe4db84bea
Duc Do <dotrananhduc@gmail.com>
2017-04-02 02:04:23 +0000

    fix and done project. Close

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 var express = require("express");
+var moment = require("moment");
 var app = express()
 
 function parsetime(time) {
+  
   // Parse time as ISO
-  return {
-    hour: time.getHours(),  
-    minute: time.getMinutes(),  
-    second: time.getSeconds()
-  };
+  return moment.unix(time).format("MMMM D, YYYY");
+  
 }
 
 function unixtime(time) {
   // Parse time as unix time
-  return {
-    unixtime : time.getTime()
-  };
+  return moment(time, "MMMM D, YYYY").format("X");
 }
 
 app.get('/', function(req, res) {
@@ -25,6 +22,17 @@ app.get('/:query', function (req, res) {
   var unix = null;
   var natural = null;
   var date = req.params.query;
+  
+  if(+date >= 0){
+    unix = +date;
+    natural = parsetime(unix);
+  }
+  
+  if(isNaN(+date) && moment(date, "MMMM D, YYYY").isValid()){
+    unix = +unixtime(date);
+    natural = parsetime(unix);
+  }
+  
   var rs = {"unix": unix, "natural": natural};
   res.send(rs);
 })
